4863f3f86d20936fd90df15f5d43dc5154afb65a
Yuriy108 <49155848+Yuriy108@users.noreply.github.com>
2020-03-12 14:30:52 +0200

    Initial commit one

---
+++ app/src/main/java/com/example/database/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+     EditText name;
+     EditText surname;
+     Button buttonadd;
+     Button buttonClear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+
+    public void showDB(View view) {
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER)");
+        db.execSQL("INSERT INTO users VALUES ('Tom Smith', 23);");
+        db.execSQL("INSERT INTO users VALUES ('John Dow', 31);");
+
+        Cursor query = db.rawQuery("SELECT * FROM users;", null);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        if(query.moveToFirst()){
+            do{
+                String name = query.getString(0);
+                int age = query.getInt(1);
+                textView.append("Name: " + name + " Age: " + age + "\n");
+            }
+            while(query.moveToNext());
+        }
+        query.close();
+        db.close();
+    }
+
+    }
+
